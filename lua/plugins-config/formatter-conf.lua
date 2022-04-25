19852e1a2c9c04be86eca21097ed7a29d01b278e
Kisto17 <vuwin24680@gmail.com>
2022-04-25 08:51:46 +0700

    token test

--- lua/plugins-config/formatter-conf.lua
+++ lua/plugins-config/formatter-conf.lua
@@ -1,10 +1,11 @@
 local M = {}
 function M.setup()
-	local formatters = require("lvim.lsp.null-ls.formatters")
-	formatters.setup({
-		{
-			exe = ".stylua",
-		},
-	})
+  local formatters = require("lvim.lsp.null-ls.formatters")
+  formatters.setup({
+    {
+      exe = ".stylua",
+    },
+  })
 end
+
 return M
