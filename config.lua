5b54e3523d46feee268a1ab75e0af8b972e04ff9
Kisto17 <vuwin24680@gmail.com>
2022-05-19 18:12:32 +0700

    clean up the config file and changed toggleterm to horizontal

--- config.lua
+++ config.lua
@@ -7,23 +7,13 @@ lvim.leader = "space"
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 
-lvim.builtin.which_key.mappings["t"] = {
-  name = "+Trouble",
-  r = { "<cmd>Trouble lsp_references<cr>", "References" },
-  f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
-  d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnostics" },
-  q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
-  l = { "<cmd>Trouble loclist<cr>", "LocationList" },
-  w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnostics" },
-}
-
 --find and replace
 lvim.builtin.which_key.mappings["r"] = {
-  name = "Find and replace",
-  s = {
-    "viw:lua require('spectre').open_file_search()<cr>",
-    "just run",
-  },
+	name = "Find and replace",
+	s = {
+		"viw:lua require('spectre').open_file_search()<cr>",
+		"just run",
+	},
 }
 
 -- TODO: User Config for predefined plugins
@@ -35,17 +25,17 @@ lvim.builtin.nvimtree.show_icons.git = 0
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
-  "bash",
-  "c",
-  "javascript",
-  "json",
-  "lua",
-  "python",
-  "typescript",
-  "css",
-  "rust",
-  "java",
-  "yaml",
+	"bash",
+	"c",
+	"javascript",
+	"json",
+	"lua",
+	"python",
+	"typescript",
+	"css",
+	"rust",
+	"java",
+	"yaml",
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
@@ -53,42 +43,46 @@ lvim.builtin.treesitter.highlight.enabled = true
 
 -- Additional Plugins
 lvim.plugins = {
-  --AutoSave
-  {
-    "Pocco81/AutoSave.nvim",
-    config = require("plugins-config.autosave-conf").setup(),
-  },
-  --Jaq aka CodeRunner
-  {
-    "is0n/jaq-nvim",
-    config = require("plugins-config.jaq-conf").setup(),
-  },
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    config = require("plugins-config.indent-blankline-conf").setup(),
-  },
-  {
-    "folke/trouble.nvim",
-    cmd = "TroubleToggle",
-  },
-  {
-    "ray-x/lsp_signature.nvim",
-    require("lsp_signature").setup(),
-  },
-  {
-    "norcalli/nvim-colorizer.lua",
-    require("colorizer").setup(),
-  },
-  {
-    "simrat39/symbols-outline.nvim",
-  },
-  {
-    "wfxr/minimap.vim",
-  },
-  {
-    "windwp/nvim-spectre",
-    require("spectre").setup(),
-  },
+	--AutoSave
+	{
+		"Pocco81/AutoSave.nvim",
+		config = require("plugins-config.autosave-conf").setup(),
+	},
+	--Jaq aka CodeRunner
+	{
+		"is0n/jaq-nvim",
+		config = require("plugins-config.jaq-conf").setup(),
+	},
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		config = require("plugins-config.indent-blankline-conf").setup(),
+	},
+	{
+		"folke/trouble.nvim",
+		cmd = "TroubleToggle",
+	},
+	{
+		"ray-x/lsp_signature.nvim",
+		require("lsp_signature").setup(),
+	},
+	{
+		"norcalli/nvim-colorizer.lua",
+		require("colorizer").setup(),
+	},
+	{
+		"simrat39/symbols-outline.nvim",
+	},
+	{
+		"wfxr/minimap.vim",
+	},
+	{
+		"windwp/nvim-spectre",
+		require("spectre").setup(),
+	},
+	{
+		"NvChad/nvterm",
+		config = require("plugins-config.nvterm-conf").setup(),
+	},
 }
 
 --Formatters
@@ -99,3 +93,6 @@ lvim.lsp.float.focusable = true
 
 -- lvim.lsp.automatic_configuration.skipped_filetyps = "javascript"
 lvim.builtin.lualine.sections.lualine_z = { "location" }
+
+lvim.builtin.terminal.direction = "horizontal"
+lvim.builtin.terminal.size = 10
